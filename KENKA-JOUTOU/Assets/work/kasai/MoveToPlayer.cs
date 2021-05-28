ce9e95117d11fd7f77bc0ffdbf5a98ac78ed030f
Nhelv5388 <20c1020010kf@edu.tech.ac.jp>
2021-05-28 18:34:43 +0900

    フェードアウトのスクリプトの内容を変更しました 敵のAIのスクリプトの内容を変更し、一度攻撃されるまでは糸を狙うようにし、攻撃された場合プレイヤーを狙うように変更しました

--- KENKA-JOUTOU/Assets/work/kasai/MoveToPlayer.cs
+++ KENKA-JOUTOU/Assets/work/kasai/MoveToPlayer.cs
@@ -5,40 +5,71 @@ using UnityEngine;
 public class MoveToPlayer : MonoBehaviour
 {
     private GameObject PlayerObject;    //プレイヤーオブジェクト
+    private GameObject WireObject;      //ワイヤーオブジェクト
     private Vector3 PlayerPosition;     //プレイヤーの位置情報
-    private Vector3 EnemyPosition;     //エネミーの位置情報
+    private Vector3 EnemyPosition;      //エネミーの位置情報
+    private Vector3 WirePosition;       //ワイヤーの位置情報
     // Start is called before the first frame update
     void Start()
     {
         PlayerObject = GameObject.FindWithTag("Player");
+        WireObject = GameObject.FindWithTag("Wire");
 
         PlayerPosition = PlayerObject.transform.position;
+        WirePosition = WireObject.transform.position;
         EnemyPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         PlayerPosition = PlayerObject.transform.position;
         EnemyPosition = transform.position;
+        WirePosition = WireObject.transform.position;
+        if (Hit.EnemyHealth == 100)
+        {
+            if (WirePosition.x > EnemyPosition.x)
+            {
+                EnemyPosition.x = EnemyPosition.x + 0.01f;
+            }
+            else if (WirePosition.x < EnemyPosition.x)
+            {
+                EnemyPosition.x = EnemyPosition.x - 0.01f;
+            }
+            if (WirePosition.z > EnemyPosition.z)
+            {
+                EnemyPosition.z = EnemyPosition.z + 0.01f;
+            }
+            else if (WirePosition.z < EnemyPosition.z)
+            {
+                EnemyPosition.z = EnemyPosition.z - 0.01f;
+            }
+            transform.position = EnemyPosition;
+        }
 
-        if (PlayerPosition.x > EnemyPosition.x)
+        else if (Hit.EnemyHealth < 100)
         {
-            EnemyPosition.x = EnemyPosition.x + 0.01f;
-        }
-        else if (PlayerPosition.x < EnemyPosition.x)
-        {
-            EnemyPosition.x = EnemyPosition.x - 0.01f;
-        }
-        if (PlayerPosition.z > EnemyPosition.z)
-        {
-            EnemyPosition.z = EnemyPosition.z + 0.01f;
-        }
-        else if (PlayerPosition.z < EnemyPosition.z)
-        {
-            EnemyPosition.z = EnemyPosition.z - 0.01f;
+            if (PlayerPosition.x > EnemyPosition.x)
+            {
+                EnemyPosition.x = EnemyPosition.x + 0.01f;
+            }
+            else if (PlayerPosition.x < EnemyPosition.x)
+            {
+                EnemyPosition.x = EnemyPosition.x - 0.01f;
+            }
+            if (PlayerPosition.z > EnemyPosition.z)
+            {
+                EnemyPosition.z = EnemyPosition.z + 0.01f;
+            }
+            else if (PlayerPosition.z < EnemyPosition.z)
+            {
+                EnemyPosition.z = EnemyPosition.z - 0.01f;
+            }
+
+            transform.position = EnemyPosition;
         }
 
-        transform.position = EnemyPosition;
+
     }
 }

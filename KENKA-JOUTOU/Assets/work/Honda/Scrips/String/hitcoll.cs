41a9d44a8179b44d64bc795f5d39a6d5644fef64
HR_GIT <nekomakuG@gmail.com>
2021-08-07 07:24:42 +0900

    牢屋をボロボロに飛ばすようにした

--- KENKA-JOUTOU/Assets/work/Honda/Scrips/String/hitcoll.cs
+++ KENKA-JOUTOU/Assets/work/Honda/Scrips/String/hitcoll.cs
@@ -8,6 +8,12 @@ public class hitcoll : MonoBehaviour
 {
     ObiSolver obi;
 
+    [SerializeField]
+    float impulse = 3;
+
+    [SerializeField]
+    float time = 3f;
+
     void Awake()
     {
         obi = GetComponent<Obi.ObiSolver>();
@@ -31,16 +37,26 @@ public class hitcoll : MonoBehaviour
             if (contact.distance < 0.01)
             {
                 ObiColliderBase col = world.colliderHandles[contact.bodyB].owner;
+                // Enemyéû
                 if (col != null && col.gameObject.CompareTag("Enemy")) 
                 {
-                    Debug.Log(col);
                     int particleIndex = obi.simplices[contact.bodyA];
 
+                    // EnemyÇÃëÃóÕÇå∏ÇÁÇµÅA0Ç…Ç»Ç¡ÇΩÇÁè¡ñ≈Ç∑ÇÈ
                     int ATK = col.gameObject.GetComponent<Skeltons>().EnemyHP -= 20;
                     if (col.gameObject.GetComponent<Skeltons>().EnemyHP <= 0)
                     {
                         Destroy(col.gameObject);
                     }
+                }
+                // òSâÆéû
+                if (col.gameObject.CompareTag("IronGrill"))
+                { 
+                    col.GetComponent<Rigidbody>().isKinematic = false;
+                    // êÅÇ¡îÚÇŒÇ∑
+                    col.GetComponent<Rigidbody>().AddForce(Vector3.forward * impulse, ForceMode.Impulse);
+                    Destroy(col.gameObject, time);
+
                 }
             }
         }

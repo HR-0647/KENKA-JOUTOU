42bf0e91e387a4a0b88bb1524e678e9afe6198ee
Nhelv5388 <20c1020010kf@edu.tech.ac.jp>
2021-09-11 19:22:31 +0900

    no message

--- KENKA-JOUTOU/Assets/work/kasai/Script/EnemySearchArea.cs
+++ KENKA-JOUTOU/Assets/work/kasai/Script/EnemySearchArea.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/// <summary>
+///このスクリプトはスケルトン用です
+/// </summary>
+
 
 public class EnemySearchArea : MonoBehaviour
 {
+    public int Area = 5;
     // Start is called before the first frame update
     void Start()
     {
-
+        this.transform.localScale = new Vector3(Area, 1, Area);
     }
 
     // Update is called once per frame

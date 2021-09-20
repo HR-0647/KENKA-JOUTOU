61c8ec9fdc63b2d3b05ea4509ed4421055cc9d5a
HR_GIT <nekomakuG@gmail.com>
2021-09-21 01:43:40 +0900

    エネミーが吹っ飛ばないようにして トラップを部屋ごとに制覇すると止まるように

--- KENKA-JOUTOU/Assets/work/Honda/Scrips/MapScript/EnemyCount.cs
+++ KENKA-JOUTOU/Assets/work/Honda/Scrips/MapScript/EnemyCount.cs
@@ -17,11 +17,15 @@ public class EnemyCount : MonoBehaviour
 
     public Animation blade1;
     public Animation blade2;
-
+           
     public Animation spike1;
     public Animation spike2;
     public Animation spike3;
     public Animation spike4;
+    public BoxCollider col1;
+    public BoxCollider col2;
+    public BoxCollider col3;
+    public BoxCollider col4;
 
     //　ホントは効率化を目指したいが今回は無し
     bool door1 = false;
@@ -56,18 +60,33 @@ public class EnemyCount : MonoBehaviour
             door2 = true;
             anim[2].Play();
             anim[3].Play();
+            FireBall1.GetComponent<Statue>().enabled = false;
+            FireBall2.GetComponent<Statue>().enabled = false;
+            FireBall3.GetComponent<Statue>().enabled = false;
+            FireBall4.GetComponent<Statue>().enabled = false;
         }
         else if (enemyObjects3.Length == 0 && !door3)
         {
             door3 = true;
             anim[4].Play();
             anim[5].Play();
+            blade1.Stop();
+            blade2.Stop();
         }
         else if (enemyObjects.Length == 0 && enemyObjects2.Length == 0 && !door4)
         {
             door4 = true;
             anim[6].Play();
+            spike1.Stop();
+            spike2.Stop();
+            spike3.Stop();
+            spike4.Stop();
+            col1.isTrigger = true;
+            col2.isTrigger = true;
+            col3.isTrigger = true;
+            col4.isTrigger = true;
         }
+
         enemyObjects = GameObject.FindGameObjectsWithTag("Enemy1");
         enemyNum = enemyObjects.Length;
         enemyObjects2 = GameObject.FindGameObjectsWithTag("Enemy2");

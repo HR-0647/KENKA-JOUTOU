2510a8b8b4a38db43ab95ba91e497e5471b8b7e9
DESKTOP-PV8OQTT\airhi <airhideplus1223@icloud.com>
2021-09-21 06:48:32 +0900

    シナリオ

---
+++ KENKA-JOUTOU/Assets/work/tomita/Prolouge/Prolouge.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Prolouge : MonoBehaviour
+{
+    public Text text;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            text.text = "“ñl‚Í–‚‰¤‚ğ“|‚·‚×‚­–‚‰¤é‚ÌÅ[•”‚Ü‚Å—ˆ‚½‚ªA–‚‰¤‚É‚ ‚Æˆê•à‹y‚Î‚¸”s–k‚µ‚Ä‚µ‚Ü‚Á‚½B‚»‚ÌŒã“ñl‚Íô‚¢‚ğ‚©‚¯‚ç‚êA˜S‰®‚É•Â‚¶‚ß‚ç‚ê‚Ä‚µ‚Ü‚Á‚½B";
+
+        }
+        
+    }
+}
+
+
+
